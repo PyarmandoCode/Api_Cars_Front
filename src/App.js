aed6acb7edca9c665bd49b18c506a1cd713ac4b7
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-09-09 13:45:15 -0500

    Front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+//Hooks
+import React, { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Esto es un componente
+//JSX
+const ApiComponent = () => {
+  const [autos, setAutos] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoaded, setisLoaded] = useState(false);
+
+
+  useEffect(() => {
+    async function CargarAutos() {
+      try {
+        const resp = await fetch('https://api-cars-jb0r.onrender.com/autos');
+        const data = await resp.json();
+        setAutos(data.results);
+        setisLoaded(true);
+
+      } catch (error) {
+        setisLoaded(true);
+        setError(error);
+      }
+    }
+    CargarAutos();
+
+
+  }, []);
+  if (error) {
+    return <><h2>{error.message}</h2></>
+  } else if (!isLoaded) {
+    return <><h1>Estamos Preparndo todo</h1></>
+  } else {
+    return (
+      <div className="container">
+        <div className="row">
+          {autos.map((item) => (
+            <div className="col-md-4" key={item.auto}>
+              <Card style={{ width: '18erm' }}>
+                <Card.Img variant="top" src={item.imagen} alt={item.nombre} />
+                <Card.Body>
+                  <Card.Title>{item.nombre}</Card.Title>
+                  <Card.Text>{item.precio}</Card.Text>
+                  <Button variant="primary">Ver Detalle</Button>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default ApiComponent;
